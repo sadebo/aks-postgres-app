9188e91c472557f3bebf9792ca252e0a7ce57956
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 08:44:08 -0400

    removes sp again

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,9 +11,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
