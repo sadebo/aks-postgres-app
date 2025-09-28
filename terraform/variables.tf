0018b3fd5d7dd5061a998b2d3e169c774721f59b
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 08:39:09 -0400

    remove ids

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,25 +33,3 @@ variable "node_size" {
   type        = string
   default     = "Standard_B2s"
 }
-
-# Service Principal authentication
-variable "client_id" {
-  description = "Service Principal client ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service Principal client secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
