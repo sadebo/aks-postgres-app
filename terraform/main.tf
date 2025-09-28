38eacb987a6a65194b773c422e85c7a59d1e4190
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 13:16:53 -0400

    hardcode acr name

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
+  name                = "parallelacr987601" # 👈 hardcoded, globally unique
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
