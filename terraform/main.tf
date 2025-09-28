0ccb94a05f87def8c8a53f8f258630d0240769a3
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 00:30:20 -0400

    fix acr

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,18 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# output "kube_config" {
-#   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-#   sensitive = true
-# }
+resource "azurerm_container_registry" "acr" {
+  name                = "myaksacr01" # 👈 static name, must be globally unique
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Allow AKS to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,8 @@ output "kube_config" {
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
 }
+
+output "acr_login_server" {
+  description = "ACR login server"
+  value       = azurerm_container_registry.acr.login_server
+}
