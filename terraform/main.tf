3f7799fbff4b5e2fb6c59512317d0d927547ad9e
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 00:10:22 -0400

    removes kube_config

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
+# output "kube_config" {
+#   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+#   sensitive = true
+# }
