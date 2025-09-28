8751034f692d9f9d765cee9ee669fb46a463fe05
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 19:35:50 -0400

    remove sub id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
 variable "client_id" {
-  description = "Service Principal client ID"
+  description = "Service Principal client ID (optional, defaults to ARM_CLIENT_ID env var)"
   type        = string
+  default     = null
 }
 
 variable "client_secret" {
-  description = "Service Principal client secret"
+  description = "Service Principal client secret (optional)"
   type        = string
+  default     = null
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID (optional)"
+  type        = string
+  default     = null
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID (optional)"
+  type        = string
+  default     = null
 }
