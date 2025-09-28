84f4f241fad2c7bd99536745818ab9079cc8f7e6
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 19:18:43 -0400

    addon

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -1,14 +1,14 @@
-# Re-query cluster AFTER it exists
+###############################################
+# Get Cluster Connection
+###############################################
 data "azurerm_kubernetes_cluster" "cluster" {
   name                = azurerm_kubernetes_cluster.aks.name
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-data "azurerm_kubernetes_cluster_credentials" "creds" {
-  name                = azurerm_kubernetes_cluster.aks.name
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
+###############################################
+# Kubernetes Provider
+###############################################
 provider "kubernetes" {
   host                   = data.azurerm_kubernetes_cluster.cluster.kube_config[0].host
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
@@ -16,6 +16,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
 }
 
+###############################################
+# Helm Provider
+###############################################
 provider "helm" {
   kubernetes {
     host                   = data.azurerm_kubernetes_cluster.cluster.kube_config[0].host
@@ -25,23 +28,27 @@ provider "helm" {
   }
 }
 
+###############################################
 # Ingress NGINX
+###############################################
 resource "helm_release" "nginx_ingress" {
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
   create_namespace = true
 }
 
+###############################################
 # ArgoCD
+###############################################
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
   create_namespace = true
-  version    = "5.51.6"
+  version          = "5.51.6"
 
   values = [
     <<EOT
@@ -52,14 +59,16 @@ EOT
   ]
 }
 
-# Cert-manager (via Helm)
+###############################################
+# Cert-manager
+###############################################
 resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace  = "cert-manager"
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
   create_namespace = true
-  version    = "1.15.0"
+  version          = "1.15.0"
 
   set {
     name  = "installCRDs"
@@ -67,11 +76,13 @@ resource "helm_release" "cert_manager" {
   }
 }
 
+###############################################
 # Postgres Operator
+###############################################
 resource "helm_release" "postgres_operator" {
-  name       = "postgres-operator"
-  repository = "https://opensource.zalando.com/postgres-operator/charts/postgres-operator"
-  chart      = "postgres-operator"
-  namespace  = "postgres-operator"
+  name             = "postgres-operator"
+  repository       = "https://opensource.zalando.com/postgres-operator/charts/postgres-operator"
+  chart            = "postgres-operator"
+  namespace        = "postgres-operator"
   create_namespace = true
 }
