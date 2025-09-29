f598dcc619b76d8dd930bf16c54310c9eca9080c
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 22:47:12 -0400

    addon issues

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -5,7 +5,6 @@ data "azurerm_kubernetes_cluster" "cluster" {
   name                = azurerm_kubernetes_cluster.aks.name
   resource_group_name = azurerm_resource_group.rg.name
 }
-
 ###############################################
 # Kubernetes Provider
 ###############################################
