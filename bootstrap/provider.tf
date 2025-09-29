5c2c03595a777db81ae206b6c0908bbf16aaf510
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-28 23:32:01 -0400

    addon

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -8,7 +8,6 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
   features {}
   subscription_id      = "5aab73eb-9b8f-4576-a059-31aa70917429"
--- terraform/addons.tf
+++ terraform/addons.tf
@@ -5,6 +5,7 @@ data "azurerm_kubernetes_cluster" "cluster" {
   name                = azurerm_kubernetes_cluster.aks.name
   resource_group_name = azurerm_resource_group.rg.name
 }
+
 ###############################################
 # Kubernetes Provider
 ###############################################
